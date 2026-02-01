48005bb9733d3579da74f270ed69338561ee6c6c
Rushil Chopra <rushilchopra@gmail.com>
2026-02-01 10:19:21 -0800

    fixx

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,10 @@
-import { handle } from '@hono/node-server/vercel'
+import { handle } from 'hono/vercel'
 import app from '../src/index'
 
 export const config = {
-  runtime: 'nodejs',
-  maxDuration: 30,
+  api: {
+    bodyParser: false,
+  },
 }
 
 export default handle(app)
